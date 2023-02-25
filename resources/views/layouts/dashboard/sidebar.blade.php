aafe2e4c9a49934b11b0cad8176583bed3832edb
rdmrajapakshe <rdmrajapakshe@gmail.com>
2023-02-25 17:54:48 +0530

    modified:   resources/views/layouts/dashboard/sidebar.blade.php

--- resources/views/layouts/dashboard/sidebar.blade.php
+++ resources/views/layouts/dashboard/sidebar.blade.php
@@ -17,7 +17,7 @@
         <ul class="nav">
             <li class="nav-item nav-category">Main</li>
             <li class="nav-item">
-                <a href="/dashboard" class="nav-link">
+                <a href="{{ route('home') }}" class="nav-link">
                     <i class="link-icon" data-feather="box"></i>
                     <span class="link-title">Dashboard</span>
                 </a>
