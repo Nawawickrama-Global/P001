509479cb56b509b67a2de8b0dee940ccdbc2e16f
didewinuwara2 <piyuma.net@gmail.com>
2023-04-05 10:04:47 +0530

    E31

--- resources/views/dashboard/product/view.blade.php
+++ resources/views/dashboard/product/view.blade.php
@@ -87,7 +87,7 @@
                                                 <tr style="border: 0">
                                                     <td style="padding:0 2px ; border: 0">
                                                         {{-- view --}}
-                                                        <button type="button" class="btn btn-warning btn-sm btn-icon" title="View">
+                                                        <button type="button" class="btn btn-warning btn-sm btn-icon" title="View" data-toggle="modal" data-target="#productView">
                                                             <i data-feather="eye"></i>
                                                         </button>
                                                     </td>
@@ -115,6 +115,59 @@
                                 @endforeach
                             </tbody>
                         </table>
+                        <div class="modal fade" id="productView" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                              <div class="modal-content">
+                                <div class="modal-header">
+                                  <h5 class="modal-title" id="exampleModalLabel">Product Name</h5>
+                                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                  </button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="product-feature-image">
+                                        <img src="" alt="" style="width: 100%">
+                                    </div>
+                                    <div class="product-details">
+                                        <p> <span style="font-weight: 800 ;padding-right:5px">SKU</span> 12345 </p>
+                                        <p> <span style="font-weight: 800 ;padding-right:5px">Specification</span> 12345 </p>
+                                        <p> <span style="font-weight: 800 ;padding-right:5px">Shiping</span> 12345 </p>
+                                        <p> <span style="font-weight: 800 ;padding-right:5px">Product Sheet</span> 12345 </p>
+                                        <p> <span style="font-weight: 800 ;padding-right:5px">Clean and Care</span> 12345 </p>
+                                        <p> <span style="font-weight: 800 ;padding-right:5px">Status</span> <span class="badge badge-secondary">Active</span></p>
+                                        <p> <span style="font-weight: 800 ;padding-right:5px">Brand</span> <span class="badge badge-secondary">Active</span></p>
+                                        <p> <span style="font-weight: 800 ;padding-right:5px">Category</span> <span class="badge badge-secondary">Active</span></p>
+                                        <p> <span style="font-weight: 800 ;padding-right:5px">Selected Attributes</span> 
+                                            <span class="badge badge-secondary">attribute 1</span>
+                                            <span class="badge badge-secondary">attribute 2</span>
+                                            <span class="badge badge-secondary">attribute 3</span>
+                                        </p>
+                                        <hr>
+                                        <span style="font-weight: 800 ;padding-right:5px">Custom Sizes</span> 
+                                        <table class="table table-bordered">
+                                            <tbody>
+                                              <tr> 
+                                                <th scope="row">XL</th>
+                                                <td>200</td>
+                                                <td>12</td>
+                                              </tr>
+                                              <tr>
+                                                <th scope="row">M</th>
+                                                <td>300</td>
+                                                <td>100</td>
+                                              </tr>
+                                            </tbody>
+                                          </table>
+
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          
                     </div>
                 </div>
             </div>
