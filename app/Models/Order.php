df8cf808e3e31e7d6369a9b02389fbeb2327e26c
rdmrajapakshe <rdmrajapakshe@gmail.com>
2023-04-09 00:00:50 +0530

    Order detail page

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
@@ -30,4 +31,9 @@ class Order extends Model
     public function user(): BelongsTo{
         return $this->belongsTo(User::class, 'user_id');
     }
+
+    public function items(): HasMany{
+        return $this->hasMany(User::class, 'order_id', 'order_id');
+    }
+
 }
